e36515a2f8cf2d216a9864475f5b55832c6b88f7
melanyhowell <melany.r.howell@gmail.com>
2023-03-28 21:39:26 +0000

    continued work on recipe form trying to work on solution for selcting categories

--- munch/static/js/script.js
+++ munch/static/js/script.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let nav_menu = document.querySelectorAll('.sidenav');
     M.Sidenav.init(nav_menu);
   });
+
+// recipe form drop down selction
+  document.addEventListener('DOMContentLoaded', function() {
+    let category_name_selector = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(category_name_selector);
+  });
