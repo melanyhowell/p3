e496f2f9fdad823966b3d174ec09e7b55fcb4c9e
melanyhowell <melany.r.howell@gmail.com>
2023-03-28 16:21:08 +0000

    adding in header and footer for template

---
+++ munch/static/js/script.js
@@ -0,0 +1,5 @@
+// nav bar initialization
+document.addEventListener('DOMContentLoaded', function() {
+    let nav_menu = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(nav_menu);
+  });
